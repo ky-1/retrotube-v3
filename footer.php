4279af052929a8e19bc2fdacbd1ef7fbb671417f
ky-1 <127993851+ky-1@users.noreply.github.com>
2023-03-30 21:56:56 -0400

    real guh

---
+++ footer.php
@@ -0,0 +1,34 @@
+<style type="text/css">
+.footer {
+background: #EEEEEE;
+width: 800px;
+padding:10px 5px;
+border-top: 1px solid gray;
+border-bottom: 1px solid gray;
+}
+
+.fareds {
+margin-top: -37px;
+}
+
+.igoogle {
+margin-right: 610px;
+}
+.lofsdds {
+margin-left: 610px;
+margin-bottom: -42px;
+}
+</style>
+<center>
+<span class="footer" style="
+    font-size: 13px;
+"><div class="lofsdds">
+<img class="ftc" src="FTC.png">
+</div><img class="igoogle" src="addtoigoogle.png"><center><div class="fareds"></img><input type="text" placeholder=""> <select class="search-type" name="search_type">
+						<option value="">Videos</option>
+						<option value="search_users">Channels</option>
+					</select> <button type="button">Search</button></center></div>
+			</span>
+</center><br>
+<center><p style="font-size: 10px;">© 2007 RETROTube</p></center>
+<script src="js/themes-global.js"></script>
