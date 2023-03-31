2bdedaa819171c36250b6bd726242ee8432d6876
ky-1 <127993851+ky-1@users.noreply.github.com>
2023-03-30 21:58:41 -0400

    ok

--- upload.php
+++ upload.php
@@ -34,7 +34,7 @@
                 <div class="card-header">Uploading Rules:</div>
                 <ul>
                     <li>NO GORE/NSFW (video will be deleted)</li>
-                    <li>NO ILLEGAL VIDEOS (permenant ip ban off of retrotube.ml)</li>
+                    <li>NO ILLEGAL VIDEOS (permanant ip ban off of retrotube.ml)</li>
                     <li>VIDEO METADATA IS NOT CORRUPTED (video will be deleted)</li>
                 </ul>
                 Thanks.
@@ -42,45 +42,85 @@
         </div>
     </div>
     <?php
-    if (isset($_POST['submit'])){
-        if(!isset($_SESSION['profileuser3'])) {
-            die("login to upload videos...");
-        }
-        $target_dir = "./videos/";
-        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-        if(!is_dir($target_dir)){
-            mkdir($target_dir);
-        }
-        $uploadOk = 1;
-        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-        if (file_exists($target_file)) {
-            echo "video with the same filename already exists.";
-            $uploadOk = 0;
-        }
-        if($imageFileType != "mp4") {
-            echo "MP4 files only.";
-            $uploadOk = 0;
-        }
-        if ($uploadOk == 0) {
-            echo "unknown error.";
-        } else {
-            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-                $statement = $mysqli->prepare("INSERT INTO videos (videotitle, description, author, filename, date) VALUES (?, ?, ?, ?, now())");
-                $statement->bind_param("ssss", $videotitle, $description, $author, $filename);
-                $videotitle = htmlspecialchars($_POST['videotitle']);
-                $description = str_replace(PHP_EOL, "<br>", htmlspecialchars($_POST['bio']));
-                $author = htmlspecialchars($_SESSION['profileuser3']);
-                $filename = basename($_FILES["fileToUpload"]["name"]);
-                $statement->execute();
-                $statement->close();
-                header("Location: .");
-            } else {
-                echo "error!!!!!!!!!!!!!!!!!! error code: ";
-                echo $_FILES["fileToUpload"]["error"];
-            }
-        }
-    }
-    ?>
+   if (isset($_POST['submit'])){
+       if(!isset($_SESSION['profileuser3'])) {
+           die("Login to upload videos...");
+       }
+       function randstr($len, $charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-"){
+           return substr(str_shuffle($charset),0,$len);
+       }
+       $v_id = randstr(11);
+       $target_dir = "content/tmp/";
+       $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+       if(!is_dir($target_dir)){
+           mkdir($target_dir);
+       }
+       $uploadOk = 1;
+       $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+       if (file_exists($target_file)) {
+           echo "
+           <div class='alert-message error page-alert'>
+           Video with the same filename already exists.
+           </div>
+           ";
+           $uploadOk = 0;
+       }
+       if($imageFileType != "mp4") {
+           echo "
+           <div class='alert-message error page-alert'>
+           MP4 files only.
+           </div>
+           ";
+           $uploadOk = 0;
+       }
+       if ($uploadOk == 0) {
+           echo "
+           <div class='alert-message error page-alert'>
+           Unknown error.
+           </div>
+           ";
+       } else {
+           if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+               rename("$target_file", "content/video/$v_id.mp4");
+               $new_target_file = "content/video/$v_id.mp4";
+               $ffmpeg = "C:/ffmpeg.exe";
+               $target_thumb = "content/thumb/".$v_id.".jpg";
+               $thumbcmd = "$ffmpeg -i $new_target_file -vf \"thumbnail\" -frames:v 1 -s 120x70 $target_thumb";
+               $video = $_POST['videotitle'];
+               $user = $_SESSION['profileuser3'];
+             //  $v_id = randstr(11);
+               $statement = $link->prepare("INSERT INTO videos (videotitle, vid, description, author, filename, thumb, date) VALUES (?, ?, ?, ?, ?, ?, now())");
+               $statement->bind_param("ssssss", $videotitle, $v_id, $description, $author, $filename, $thumb);
+               $videotitle = htmlspecialchars($_POST['videotitle']);
+               $description = str_replace(PHP_EOL, "<br>", htmlspecialchars($_POST['bio']));
+               $author = htmlspecialchars($_SESSION['profileuser3']);
+               $filename = "$v_id.mp4";
+               $thumb = "$v_id.jpg";
+               exec($thumbcmd);
+               $statement->execute();
+               $statement->close();
+               $webhookurl = $webhook;
+               $msg = "**$user** just uploaded **$video** => uploaded to a private test instance";
+               $json_data = array ('content'=>"$msg");
+               $make_json = json_encode($json_data);
+               $ch = curl_init( $webhookurl );
+               curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+               curl_setopt( $ch, CURLOPT_POST, 1);
+               curl_setopt( $ch, CURLOPT_POSTFIELDS, $make_json);
+               curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
+               curl_setopt( $ch, CURLOPT_HEADER, 0);
+               curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+               $response = curl_exec( $ch );
+               echo('<script>
+             window.location.href = "index.php?msg=Your video has been uploaded!";
+             </script>');
+           } else {
+               echo "The upload failed. Error code: ";
+               echo $_FILES["fileToUpload"]["error"];
+           }
+       }
+   }
+   ?>
     <hr>
     <?php include("footer.php") ?>
 </body>
