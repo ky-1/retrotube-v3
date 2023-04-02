4634f135a1ce2abd8c67c42a95fa116080e1751b
ky-1 <127993851+ky-1@users.noreply.github.com>
2023-04-02 18:57:02 -0400

    so silly

--- admin.php
+++ admin.php
@@ -39,6 +39,15 @@
        $phpver = phpversion();
        echo "Users: $usercount <br> Videos: $videocount <br> Comments: $commentcount <br> PHP Version: $phpver<br>";
        ?>
+       <?php
+function convert($size)
+ {
+    $unit=array('b','kb','mb','gb','tb','pb');
+    return @round($size/pow(1024,($i=floor(log($size,1024)))),2).' '.$unit[$i];
+ }
+echo 'Memory usage: ';
+echo convert(memory_get_usage(true)); // 123 kb
+?>
     <?php include("footer.php") ?>
 </body>
 <?php $mysqli->close();?>
