c0b02c63d2ef46763c5e69307542fccd0409241c
ky-1 <127993851+ky-1@users.noreply.github.com>
2023-04-03 11:07:18 -0400

    comical even lmao

--- channels.php
+++ channels.php
@@ -9,16 +9,17 @@
 		<div class="container-flex">
 	    <div class="col-2-3">
 			<?php
-			$statement = $mysqli->prepare("SELECT * FROM users ORDER BY subscribers DESC");
+			$statement = $mysqli->prepare("SELECT * FROM users ORDER BY username DESC");
 			$statement->execute();
 			$result = $statement->get_result();
 			if($result->num_rows !== 0){
 				while($row = $result->fetch_assoc()) {
+					$rows = getSubscribers($row['username'], $mysqli);
 				    echo "
 				    <div class='user'>
 				    	<div class='user-info'>
 						    <div><a href='./profile.php?user=".$row['username']."'>".$row['username']."</a></div>
-						    <div><span class='black'>".$row['subscribers']."</span> subscribers</div>
+						    <div><span class='black'>".$rows."</span> subscribers</div>
 					    </div>
 					    <div><a href='./profile.php?user=".$row["username"]."'><img class='user-picture' src='./pfp/".getUserPic($row["id"])."'></a></div>
 				    </div>
